6d2a3698b16b11e5ca7c19678d1b4690009796ca
devdmitchell <dmitchell.prsvr@gmail.com>
2024-12-16 11:05:07 -0600

    added images and updated divs to buttons

--- main.js
+++ main.js
@@ -1,2 +1,16 @@
 
-  
+const song1 = newAudio('')
+let isPlaying = false
+
+document.getElementById('play').addEventListener('click', () => {
+    if(isPlaying) {
+        song1.play();
+        isPlaying = true
+    }})
+
+    document.getElementById('stop').addEventListener('click', () => {
+        if(isPlaying) {
+            song1.pause()
+            song1.currentTime = 0
+            isPlaying = false
+        }})
